d0f36f7502ac35c2ee14a941de8309833b8f8ec0
renzengtao <renzengtao@autohome.com.cn>
2017-10-12 15:26:05 +0800

    update

--- src/main/java/dataStructure/sort/DirectInsertSort.java
+++ src/main/java/dataStructure/sort/DirectInsertSort.java
@@ -7,23 +7,25 @@ public class DirectInsertSort  extends   Sort{
 
     /**
      * 直接插入排序
+     * 前面是有序的，后面是无序的，把后面的一个个插入到前面
      * @param elements
      */
     public static void sort(int[] elements) {
-        for (int i = 1; i < elements.length; i++) {
-            int currentNumber = elements[i];
-            int j = i - 1;
-            while (j >= 0 && elements[j] > currentNumber) {
-                compareCount++;
-                elements[j + 1] = elements[j];
-                j--;
-            }
-            elements[j + 1] = currentNumber;
-        }
+       for(int i = 1 ;i<elements.length;i++){
+           if(elements[i] < elements[i-1]){
+               int tmp = elements[i];
+               int j = i-1;
+               for(j = i-1;j>=0&&elements[j] > tmp ;j--){
+                   elements[j+1] = elements[j];
+                   printArray(elements);
+               }
+               elements[j+1] = tmp;
+           }
+       }
     }
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 2, 3, 4, 6, 0, 9, 100, 78, 93, 88, 66, 51};
+        int[] array = {1,3,4,2};
         sort(array);
         printArray(array);
         System.out.println("swap : " + Sort.swapCount);
